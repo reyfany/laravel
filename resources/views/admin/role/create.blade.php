3ddd30af2e4677141a60d472c641f515220b830b
DESKTOP-JPJ84BP\SAMSUNG <wahyu.re38@gmail.com>
2021-06-15 06:58:13 +0700

    Permission input library cdnjs DONE

--- resources/views/admin/role/create.blade.php
+++ resources/views/admin/role/create.blade.php
@@ -7,8 +7,21 @@
 <link rel="stylesheet" href="{{asset('public/vendors/themify-icons/css/themify-icons.css')}}">
 <link rel="stylesheet" href="{{asset('public/vendors/flag-icon-css/css/flag-icon.min.css')}}">
 <link rel="stylesheet" href="{{asset('public/vendors/selectFX/css/cs-skin-elastic.css')}}">
-
 <link rel="stylesheet" href="{{asset('public/assets/css/style.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"> </script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script> 
+
+<script>
+    $(document).ready(function(){
+        $(".mul-select").select2({
+            placeholder: "Pilih permission .....",
+            tags: true,
+            tokenSeparators: ['/',',',';',' '],
+            width: "100%"
+        });
+      });
+</script>
 
 <div class="breadcrumbs">
     <div class="col-sm-4">
@@ -56,11 +69,8 @@
                         </div>
 
                         @endif
-                        <!-- Untuk Form menuju ke route dulu baru contller tugas store -->
                         <form action="{{route('roles.store')}}" method="post" enctype="multipart/form-data" class="form-horizontal"> 
-                            @csrf
-                            <!-- Untuk Form -->
-
+                            @csrf                          
                             <div class="row form-group">
                                 <div class="col col-md-3"><label for="text-input" class=" form-control-label">Nama Role</label></div>
                                 <div class="col-12 col-md-9"><input type="text" id="text-input" name="txtnama_role" placeholder="Text" class="form-control"><small class="form-text text-muted"></small></div>
@@ -69,9 +79,9 @@
                             <div class="row form-group">
                                 <div class="col col-md-3"><label for="select" class=" form-control-label">Permission</label></div>
                                 <div class="col-12 col-md-9">
-                                    <select name="optionid_permission[]" id="select" class="form-control" multiple="true">
+                                    <select name="optionid_permission[]" id="select" class="mul-select" multiple="true">
                                         @foreach($allPermission as $permission) 
-                                        <option value="{{$permission->id}}">
+                                        <option value={{$permission->id}}>
                                         {{$permission->name}}</option> 
                                         @endforeach
                                     </select>
@@ -96,8 +106,8 @@
     </div>
 </div>
 
-<!-- <script src="{{asset('public/vendors/jquery/dist/jquery.min.js')}}"></script>
-<script src="{{asset('public/vendors/popper.js/dist/umd/popper.min.js')}}"></script> -->
+<script src="{{asset('public/vendors/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{asset('public/vendors/popper.js/dist/umd/popper.min.js')}}"></script>
 
 <script src="{{asset('public/vendors/jquery-validation/dist/jquery.validate.min.js')}}"></script>
 <script src="{{asset('public/vendors/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js')}}"></script>
