2a5b72860ca8fd054b4eb7397e7ef884568739e6
reyfany <wahyu.re38@gmail.com>
2021-09-22 06:50:15 +0700

    tambahan api login

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -8,15 +8,18 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
-    public function register(Request $request){
-        $validateData = $request->validate([
+    public function register(Request $request)
+    {
+        $validateData = $request->validate
+        ([
                 'name' => 'required|max:25',
                 'email' => 'email | required | unique:users',
                 'password' => 'required | confirmed',
         ]);
 
         // create user
-        $user = new User ([
+        $user = new User
+        ([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
@@ -25,5 +28,37 @@ class AuthController extends Controller
         $user->save;
 
         return response()->json($user, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validateData = $request->validate
+        ([
+            'email' => 'email | required | unique:users',
+            'password' => 'required | confirmed',
+        ]);
+
+        $login_detail = request(['email','password']);
+
+        if(!Auth::attempt($login_detail))
+        {
+            return response()->json([
+                'error' => 'login gagal. Cek lagi detail login'
+            ], 401);
+        }
+
+        $user= $request->user();
+
+        $tokenResult = $user->createToken('AccessToken');
+        $token = $tokenResult->token;
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_id' => $token->id,
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email
+        ], 200);
     }
 }
